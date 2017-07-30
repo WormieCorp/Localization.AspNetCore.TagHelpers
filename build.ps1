aca46b1e577d37fa153949a8d5e15bc770e1812e
AdmiringWorm <kim.nordmo@gmail.com>
2017-07-30 17:08:48 +0200

    Implemented building using Cake.Recipe
    fixes #20

--- build.ps1
+++ build.ps1
@@ -40,7 +40,7 @@ http://cakebuild.net
 
 [CmdletBinding()]
 Param(
-  [string]$Script = "build.cake",
+  [string]$Script = "setup.cake",
   [string]$Target = "Default",
   [ValidateSet("Release", "Debug")]
   [string]$Configuration = "Release",
