4dca9a1e4cae3745d323b8fb4d4ad0e04bf0d286
AdmiringWorm <kim.nordmo@gmail.com>
2018-04-07 12:02:57 +0200

    (build) Added missing solutioninfo file

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
+[assembly: AssemblyInformationalVersion("0.4.0")]
+[assembly: AssemblyCopyright("Copyright 2016-2018 - Kim Nordmo")]
