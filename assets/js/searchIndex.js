dfdf1f68eaf19e21e5234bb332b2ec9b808ccd3c
AdmiringWorm <kim.nordmo@gmail.com>
2019-03-01 11:16:48 +0000

    AppVeyor Publish: e1940e0255698e8e5a0febbb6d2a852775e9c14d (docs) Added documentation for new attribute parameter feature

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -1,161 +1,178 @@
 
-var camelCaseTokenizer = function (obj) {
+var camelCaseTokenizer = function (builder) {
+
+  var pipelineFunction = function (token) {
     var previous = '';
-    return obj.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
-        var current = cur.toLowerCase();
-        if(acc.length === 0) {
-            previous = current;
-            return acc.concat(current);
-        }
-        previous = previous.concat(current);
-        return acc.concat([current, previous]);
+    // split camelCaseString to on each word and combined words
+    // e.g. camelCaseTokenizer -> ['camel', 'case', 'camelcase', 'tokenizer', 'camelcasetokenizer']
+    var tokenStrings = token.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
+      var current = cur.toLowerCase();
+      if (acc.length === 0) {
+        previous = current;
+        return acc.concat(current);
+      }
+      previous = previous.concat(current);
+      return acc.concat([current, previous]);
     }, []);
+
+    // return token for each string
+    // will copy any metadata on input token
+    return tokenStrings.map(function(tokenString) {
+      return token.clone(function(str) {
+        return tokenString;
+      })
+    });
+  }
+
+  lunr.Pipeline.registerFunction(pipelineFunction, 'camelCaseTokenizer')
+
+  builder.pipeline.before(lunr.stemmer, pipelineFunction)
 }
-lunr.tokenizer.registerFunction(camelCaseTokenizer, 'camelCaseTokenizer')
 var searchModule = function() {
+    var documents = [];
     var idMap = [];
-    function y(e) { 
-        idMap.push(e); 
+    function a(a,b) { 
+        documents.push(a);
+        idMap.push(b); 
     }
+
+    a(
+        {
+            id:0,
+            title:"LocalizeTagHelperOptions",
+            content:"LocalizeTagHelperOptions",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/LocalizeTagHelperOptions',
+            title:"LocalizeTagHelperOptions",
+            description:""
+        }
+    );
+    a(
+        {
+            id:1,
+            title:"NewLineHandling",
+            content:"NewLineHandling",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/NewLineHandling',
+            title:"NewLineHandling",
+            description:""
+        }
+    );
+    a(
+        {
+            id:2,
+            title:"Program",
+            content:"Program",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Localization.AspNetCore.TagHelpers/api/Localization.Demo/Program',
+            title:"Program",
+            description:""
+        }
+    );
+    a(
+        {
+            id:3,
+            title:"LocalizeTagHelper",
+            content:"LocalizeTagHelper",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/LocalizeTagHelper',
+            title:"LocalizeTagHelper",
+            description:""
+        }
+    );
+    a(
+        {
+            id:4,
+            title:"HomeController",
+            content:"HomeController",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Localization.AspNetCore.TagHelpers/api/Localization.Demo.Controllers/HomeController',
+            title:"HomeController",
+            description:""
+        }
+    );
+    a(
+        {
+            id:5,
+            title:"Startup",
+            content:"Startup",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Localization.AspNetCore.TagHelpers/api/Localization.Demo/Startup',
+            title:"Startup",
+            description:""
+        }
+    );
+    a(
+        {
+            id:6,
+            title:"ParameterTagHelper",
+            content:"ParameterTagHelper",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/ParameterTagHelper',
+            title:"ParameterTagHelper",
+            description:""
+        }
+    );
+    a(
+        {
+            id:7,
+            title:"GenericLocalizeTagHelper",
+            content:"GenericLocalizeTagHelper",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/GenericLocalizeTagHelper',
+            title:"GenericLocalizeTagHelper",
+            description:""
+        }
+    );
+    a(
+        {
+            id:8,
+            title:"LocalizeAttributeTagHelper",
+            content:"LocalizeAttributeTagHelper",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/LocalizeAttributeTagHelper',
+            title:"LocalizeAttributeTagHelper",
+            description:""
+        }
+    );
     var idx = lunr(function() {
-        this.field('title', { boost: 10 });
+        this.field('title');
         this.field('content');
-        this.field('description', { boost: 5 });
-        this.field('tags', { boost: 50 });
+        this.field('description');
+        this.field('tags');
         this.ref('id');
-        this.tokenizer(camelCaseTokenizer);
+        this.use(camelCaseTokenizer);
 
         this.pipeline.remove(lunr.stopWordFilter);
         this.pipeline.remove(lunr.stemmer);
-    });
-    function a(e) { 
-        idx.add(e); 
-    }
-
-    a({
-        id:0,
-        title:"Startup",
-        content:"Startup",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:1,
-        title:"HomeController",
-        content:"HomeController",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:2,
-        title:"LocalizeTagHelper",
-        content:"LocalizeTagHelper",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:3,
-        title:"LocalizeTagHelperOptions",
-        content:"LocalizeTagHelperOptions",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:4,
-        title:"GenericLocalizeTagHelper",
-        content:"GenericLocalizeTagHelper",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:5,
-        title:"Program",
-        content:"Program",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:6,
-        title:"LocalizeAttributeTagHelper",
-        content:"LocalizeAttributeTagHelper",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:7,
-        title:"ParameterTagHelper",
-        content:"ParameterTagHelper",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:8,
-        title:"NewLineHandling",
-        content:"NewLineHandling",
-        description:'',
-        tags:''
-    });
-
-    y({
-        url:'/Localization.AspNetCore.TagHelpers/api/Localization.Demo/Startup',
-        title:"Startup",
-        description:""
-    });
-
-    y({
-        url:'/Localization.AspNetCore.TagHelpers/api/Localization.Demo.Controllers/HomeController',
-        title:"HomeController",
-        description:""
-    });
-
-    y({
-        url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/LocalizeTagHelper',
-        title:"LocalizeTagHelper",
-        description:""
-    });
-
-    y({
-        url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/LocalizeTagHelperOptions',
-        title:"LocalizeTagHelperOptions",
-        description:""
-    });
-
-    y({
-        url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/GenericLocalizeTagHelper',
-        title:"GenericLocalizeTagHelper",
-        description:""
-    });
-
-    y({
-        url:'/Localization.AspNetCore.TagHelpers/api/Localization.Demo/Program',
-        title:"Program",
-        description:""
-    });
-
-    y({
-        url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/LocalizeAttributeTagHelper',
-        title:"LocalizeAttributeTagHelper",
-        description:""
-    });
-
-    y({
-        url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/ParameterTagHelper',
-        title:"ParameterTagHelper",
-        description:""
-    });
-
-    y({
-        url:'/Localization.AspNetCore.TagHelpers/api/Localization.AspNetCore.TagHelpers/NewLineHandling',
-        title:"NewLineHandling",
-        description:""
+        documents.forEach(function (doc) { this.add(doc) }, this)
     });
 
     return {
