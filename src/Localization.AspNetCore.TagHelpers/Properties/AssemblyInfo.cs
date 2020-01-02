8bd3abd4725a5e5b9ce917aaaced521e08b598c4
AdmiringWorm <kim.nordmo@gmail.com>
2020-01-02 01:59:50 +0100

    Reflect the state of com and cls compliant

---
+++ src/Localization.AspNetCore.TagHelpers/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+// -----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="WormieCorp">
+//   Copyright (c) WormieCorp. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+// <author>Kim Nordmo</author>
+
+using System;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
